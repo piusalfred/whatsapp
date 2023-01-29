1a5d602f94191362b52975998671cb89339c6c5e
Pius Alfred <me.pius1102@gmail.com>
2023-01-29 22:15:13 +0300

    init

--- whatsapp.go
+++ whatsapp.go
@@ -1,6 +1,132 @@
 package whatsapp
 
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+// https://graph.facebook.com/v15.0/FROM_PHONE_NUMBER_ID/messages
+
+const (
+	BaseURL                = "https://graph.facebook.com/"
+	TextMessageType        = "text"
+	ReactionMessageType    = "reaction"
+	MediaMessageType       = "media"
+	LocationMessageType    = "location"
+	ContactMessageType     = "contact"
+	InteractiveMessageType = "interactive"
+)
+
 type (
+
+	// Reaction is a WhatsApp reaction
+	Reaction struct {
+		MessageID string `json:"message_id"`
+		Emoji     string `json:"emoji"`
+	}
+
+	// ReactionMessage is a WhatsApp reaction message
+	// If the message you are reacting to is more than 30 days old, doesn't correspond to
+	// any message in the conversation, has been deleted, or is itself a reaction message,
+	// the reaction message will not be delivered and you will receive a webhook with the
+	// code 131009.
+	//'{
+	//   "messaging_product": "whatsapp",
+	//   "recipient_type": "individual",
+	//   "to": "PHONE_NUMBER",
+	//   "type": "reaction",
+	//   "reaction": {
+	//     "message_id": "wamid.HBgLM...",
+	//     "emoji": "\uD83D\uDE00"
+	//   }
+	// }'
+	ReactionMessage struct {
+		Product       string    `json:"messaging_product"`
+		RecipientType string    `json:"recipient_type"`
+		To            string    `json:"to"`
+		Type          string    `json:"type"`
+		Reaction      *Reaction `json:"reaction"`
+	}
+
+	// Text ...
+	Text struct {
+		PreviewUrl bool   `json:"preview_url,omitempty"`
+		Body       string `json:"body,omitempty"`
+	}
+
+	// TextMessage is a WhatsApp text message
+	// //{
+	// 	"messaging_product": "whatsapp",
+	// 	"recipient_type": "individual",
+	// 	"to": "PHONE_NUMBER",
+	// 	"type": "text",
+	// 	"text": { // the text object
+	// 	  "preview_url": false,
+	// 	  "body": "MESSAGE_CONTENT"
+	// 	  }
+	//   }'
+	TextMessage struct {
+		Product       string `json:"messaging_product"`
+		RecipientType string `json:"recipient_type"`
+		To            string `json:"to"`
+		Type          string `json:"type"`
+		Text          *Text  `json:"text"`
+	}
+
+	// MessageType represents the type of message currently supported.
+	// Which are Text messages,Reaction messages,Media messages,Location messages,Contact messages,
+	// and Interactive messages.
+	// You may also send any of these message types as a reply, with the exception of reaction messages.
+	// For more go to https://developers.facebook.com/docs/whatsapp/cloud-api/guides/send-messages
+	MessageType string
+
+	ResponseHeaders map[string][]string
+
+	// Message is a WhatsApp message
+	Message struct {
+		Product  string           `json:"messaging_product"`
+		To       string           `json:"to"`
+		Type     string           `json:"type"`
+		Template *MessageTemplate `json:"template"`
+	}
+
+	TemplateLanguage struct {
+		Code string `json:"code"`
+	}
+
+	MessageTemplate struct {
+		Name     string           `json:"name"`
+		Language TemplateLanguage `json:"language"`
+	}
+
+	// MessageResponse is a WhatsApp message response
+	// {
+	//		"messaging_product":"whatsapp",
+	//		"contacts":[
+	//				{
+	//					"input":"255767001828",
+	//					"wa_id":"255767001828",
+	//				},
+	//		],
+	//		"messages":[
+	//				{"id":"wamid.HBgMMjU1NzY3MDAxODI4FQIAERgSRjVDRDE5NjhBOEEwQzQ0NUE1AA=="},
+	//		],
+	//	}
+	MessageResponse struct {
+		MessagingProduct string `json:"messaging_product"`
+		Contacts         []struct {
+			Input      string `json:"input"`
+			WhatsappID string `json:"wa_id"`
+		} `json:"contacts"`
+		Messages []struct {
+			ID string `json:"id"`
+		} `json:"messages"`
+	}
 
 	// Response is the response from the WhatsApp server
 	//
@@ -93,4 +219,108 @@ type (
 			} `json:"sections"`
 		} `json:"action"`
 	}
+
+	// RequestParams are parameters for a request containing headers, query params,
+	// Bearer token, Method and the body.
+	// These parameters are used to create a *http.Request
+	RequestParams struct {
+		SenderID   string
+		ApiVersion string
+		Headers    map[string]string
+		Query      map[string]string
+		Bearer     string
+		BaseURL    string
+		Method     string
+	}
+
+	Sender func(ctx context.Context, client *http.Client, params *RequestParams, payload []byte) (*Response, error)
+
+	SenderMiddleware func(next Sender) Sender
 )
+
+// NewRequestWithContext creates a new *http.Request with context by using the
+// RequestParams.
+func NewRequestWithContext(ctx context.Context, params *RequestParams, payload []byte) (*http.Request, error) {
+	var (
+		req *http.Request
+		err error
+	)
+	//https://graph.facebook.com/v15.0/FROM_PHONE_NUMBER_ID/messages
+	requestURL, err := url.JoinPath(params.BaseURL, params.ApiVersion, params.SenderID, "messages")
+	if err != nil {
+		return nil, fmt.Errorf("failed to join url parts: %w", err)
+	}
+
+	if payload == nil {
+		req, err = http.NewRequestWithContext(ctx, params.Method, requestURL, nil)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create new request: %w", err)
+		}
+	} else {
+		req, err = http.NewRequestWithContext(ctx, params.Method, requestURL, bytes.NewBuffer(payload))
+		if err != nil {
+			return nil, fmt.Errorf("failed to create new request: %w", err)
+		}
+	}
+
+	for key, value := range params.Headers {
+		req.Header.Set(key, value)
+	}
+
+	if params.Bearer != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", params.Bearer))
+	}
+
+	if len(params.Query) > 0 {
+		query := req.URL.Query()
+		for key, value := range params.Query {
+			query.Add(key, value)
+		}
+		req.URL.RawQuery = query.Encode()
+	}
+
+	return req, nil
+}
+
+func Send(ctx context.Context, client *http.Client, params *RequestParams, payload []byte) (*Response, error) {
+	var (
+		req  *http.Request
+		resp *http.Response
+		err  error
+	)
+
+	if req, err = NewRequestWithContext(ctx, params, payload); err != nil {
+		return nil, err
+	}
+
+	if resp, err = client.Do(req); err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.Body == nil {
+		return nil, fmt.Errorf("empty response body")
+	}
+
+	bodybytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		var errResponse ErrorResponse
+		if err = json.Unmarshal(bodybytes, &errResponse); err != nil {
+			return nil, err
+		}
+		errResponse.Code = resp.StatusCode
+		return nil, &errResponse
+	}
+
+	var response Response
+	if err = json.NewDecoder(bytes.NewBuffer(bodybytes)).Decode(&response); err != nil {
+		return nil, err
+	}
+
+	return &response, nil
+}
