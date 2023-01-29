168a0052b2d6015c4e985f30eee9be5cf8377383
Pius Alfred <me.pius1102@gmail.com>
2023-01-30 02:08:49 +0300

    init

--- whatsapp.go
+++ whatsapp.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // https://graph.facebook.com/v15.0/FROM_PHONE_NUMBER_ID/messages
@@ -23,6 +25,19 @@ const (
 )
 
 type (
+
+	// Context used to store the context of the conversation.
+	// You can send any message as a reply to a previous message in a conversation by including
+	// the previous message's ID in the context object.
+	// The recipient will receive the new message along with a contextual bubble that displays
+	// the previous message's content.
+	// Recipients will not see a contextual bubble if:
+	//    - replying with a template message ("type":"template")
+	//    - replying with an image, video, PTT, or audio, and the recipient is on KaiOS
+	// These are known bugs which we are addressing.
+	Context struct {
+		MessageID string `json:"message_id"`
+	}
 
 	// Reaction is a WhatsApp reaction
 	Reaction struct {
@@ -359,4 +374,224 @@ func SendLocation(ctx context.Context, client *http.Client, params *RequestParam
 	}
 
 	return Send(ctx, client, params, payload)
+}
+
+/*
+React sends a reaction to a message.
+To send reaction messages, make a POST call to /PHONE_NUMBER_ID/messages and attach a message object
+with type=reaction. Then, add a reaction object.
+
+Sample request:
+
+	curl -X  POST \
+	 'https://graph.facebook.com/v15.0/FROM_PHONE_NUMBER_ID/messages' \
+	 -H 'Authorization: Bearer ACCESS_TOKEN' \
+	 -H 'Content-Type: application/json' \
+	 -d '{
+	  "messaging_product": "whatsapp",
+	  "recipient_type": "individual",
+	  "to": "PHONE_NUMBER",
+	  "type": "reaction",
+	  "reaction": {
+	    "message_id": "wamid.HBgLM...",
+	    "emoji": "\uD83D\uDE00"
+	  }
+	}'
+
+If the message you are reacting to is more than 30 days old, doesn't correspond to any message
+in the conversation, has been deleted, or is itself a reaction message, the reaction message will
+not be delivered and you will receive a webhook with the code 131009.
+
+A successful response includes an object with an identifier prefixed with wamid. Use the ID listed
+after wamid to track your message status.
+
+Example response:
+
+	{
+	  "messaging_product": "whatsapp",
+	  "contacts": [{
+	      "input": "PHONE_NUMBER",
+	      "wa_id": "WHATSAPP_ID",
+	    }]
+	  "messages": [{
+	      "id": "wamid.ID",
+	    }]
+	}
+*/
+func React(ctx context.Context, client *http.Client, params *RequestParams, reaction *Reaction) (*Response, error) {
+	payload, err := json.Marshal(reaction)
+	if err != nil {
+		return nil, err
+	}
+
+	return Send(ctx, client, params, payload)
+}
+
+// MediaType is the type of media to send it can be audio, document, image, sticker, or video.
+type MediaType string
+
+/*
+SendMediaOptions contains the options on how to send a media message. You can specify either the
+ID or the link of the media. Also it allows you to specify caching options.
+
+The Cloud API supports media HTTP caching. If you are using a link (link) to a media asset on your
+server (as opposed to the ID (id) of an asset you have uploaded to our servers),you can instruct us
+to cache your asset for reuse with future messages by including the headers below
+in your server response when we request the asset. If none of these headers are included, we will
+not cache your asset.
+
+	Cache-Control: <CACHE_CONTROL>
+	Last-Modified: <LAST_MODIFIED>
+	ETag: <ETAG>
+
+# Cache-Control
+
+The Cache-Control header tells us how to handle asset caching. We support the following directives:
+
+	max-age=n: Indicates how many seconds (n) to cache the asset. We will reuse the cached asset in subsequent
+	messages until this time is exceeded, after which we will request the asset again, if needed.
+	Example: Cache-Control: max-age=604800.
+
+	no-cache: Indicates the asset can be cached but should be updated if the Last-Modified header value
+	is different from a previous response.Requires the Last-Modified header.
+	Example: Cache-Control: no-cache.
+
+	no-store: Indicates that the asset should not be cached. Example: Cache-Control: no-store.
+
+	private: Indicates that the asset is personalized for the recipient and should not be cached.
+
+# Last-Modified
+
+Indicates when the asset was last modified. Used with Cache-Control: no-cache. If the Last-Modified value
+is different from a previous response and Cache-Control: no-cache is included in the response,
+we will update our cached version of the asset with the asset in the response.
+Example: Date: Tue, 22 Feb 2022 22:22:22 GMT.
+
+# ETag
+
+The ETag header is a unique string that identifies a specific version of an asset.
+Example: ETag: "33a64df5". This header is ignored unless both Cache-Control and Last-Modified headers
+are not included in the response. In this case, we will cache the asset according to our own, internal
+logic (which we do not disclose).
+*/
+type SendMediaOptions struct {
+	SendByLink bool
+	SendByID   bool
+	Cache      bool
+	Type       MediaType
+	Recipient  string
+	ID         string
+	Link       string
+	Expires    int
+	LastMod    string
+	ETag       string
+}
+
+const (
+	MediaTypeAudio    MediaType = "audio"
+	MediaTypeDocument MediaType = "document"
+	MediaTypeImage    MediaType = "image"
+	MediaTypeSticker  MediaType = "sticker"
+	MediaTypeVideo    MediaType = "video"
+)
+
+/*
+SendMedia sends a media message to the recipient.
+To send a media message, make a POST call to the /PHONE_NUMBER_ID/messages endpoint with
+type parameter set to audio, document, image, sticker, or video, and the corresponding
+information for the media type such as its ID or link (see Media HTTP Caching).
+
+Sample request using image with link:
+
+	curl -X  POST \
+	 'https://graph.facebook.com/v15.0/FROM-PHONE-NUMBER-ID/messages' \
+	 -H 'Authorization: Bearer ACCESS_TOKEN' \
+	 -H 'Content-Type: application/json' \
+	 -d '{
+	  "messaging_product": "whatsapp",
+	  "recipient_type": "individual",
+	  "to": "PHONE-NUMBER",
+	  "type": "image",
+	  "image": {
+	    "link" : "https://IMAGE_URL"
+	  }
+	}'
+
+Sample request using media ID:
+
+	curl -X  POST \
+	 'https://graph.facebook.com/v15.0/FROM-PHONE-NUMBER-ID/messages' \
+	 -H 'Authorization: Bearer ACCESS_TOKEN' \
+	 -H 'Content-Type: application/json' \
+	 -d '{
+	  "messaging_product": "whatsapp",
+	  "recipient_type": "individual",
+	  "to": "PHONE-NUMBER",
+	  "type": "image",
+	  "image": {
+	    "id" : "MEDIA-OBJECT-ID"
+	  }
+	}'
+
+A successful response includes an object with an identifier prefixed with wamid. If you are using a link to
+send the media, please check the callback events delivered to your Webhook server whether the media has been
+downloaded successfully.
+
+	{
+	  "messaging_product": "whatsapp",
+	  "contacts": [{
+	      "input": "PHONE_NUMBER",
+	      "wa_id": "WHATSAPP_ID",
+	    }]
+	  "messages": [{
+	      "id": "wamid.ID",
+	    }]
+	}
+*/
+func SendMedia(ctx context.Context, client *http.Client, params *RequestParams, options *SendMediaOptions) (*Response, error) {
+	if options == nil {
+		return nil, fmt.Errorf("options cannot be nil")
+	}
+
+	payload, err := BuildPayloadForMediaMessage(options)
+	if err != nil {
+		return nil, err
+	}
+
+	return Send(ctx, client, params, payload)
+}
+
+var InternalSendMediaError = errors.New("internal error while sending media")
+
+// BuildPayloadForMediaMessage builds the payload for a media message. It accepts SendMediaOptions
+// and returns a byte array and an error. This function is used internally by SendMedia.
+// if neither ID nor Link is specified, it returns an error.
+//
+// For Link requests, the payload should be something like this:
+// {"messaging_product": "whatsapp","recipient_type": "individual","to": "PHONE-NUMBER","type": "image","image": {"link" : "https://IMAGE_URL"}}
+func BuildPayloadForMediaMessage(options *SendMediaOptions) ([]byte, error) {
+	receipient := options.Recipient
+	mediaType := string(options.Type)
+	payloadBuilder := strings.Builder{}
+	payloadBuilder.WriteString(`{"messaging_product":"whatsapp","recipient_type":"individual","to":"`)
+	payloadBuilder.WriteString(receipient)
+	payloadBuilder.WriteString(`","type": "`)
+	payloadBuilder.WriteString(mediaType)
+	payloadBuilder.WriteString(`","`)
+	payloadBuilder.WriteString(mediaType)
+
+	// check if we are sending by link or by id
+	if options.SendByLink {
+		payloadBuilder.WriteString(`":{"link":"`)
+		payloadBuilder.WriteString(options.Link)
+		payloadBuilder.WriteString(`"}}`)
+	} else if options.SendByID {
+		payloadBuilder.WriteString(`":{"id":"`)
+		payloadBuilder.WriteString(options.ID)
+		payloadBuilder.WriteString(`"}}`)
+	} else {
+		return nil, errors.New("must specify either ID or Link")
+	}
+
+	return []byte(payloadBuilder.String()), nil
 }
--- whatsapp_test.go
+++ whatsapp_test.go
@@ -2,6 +2,7 @@ package whatsapp
 
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
 )
 
@@ -39,4 +40,44 @@ func TestResponseMarshalJson(t *testing.T) {
 	}
 
 	t.Log(resulr)
+}
+
+func TestBuildPayloadForMediaMessage(t *testing.T) {
+	type args struct {
+		options *SendMediaOptions
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "TestBuildPayloadForMediaMessage",
+			args: args{
+				options: &SendMediaOptions{
+					Recipient:  "123456789",
+					Type:       MediaTypeImage,
+					SendByLink: true,
+					Link:       "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+				},
+			},
+			want: `{"messaging_product": "whatsapp","recipient_type":"individual","to":"123456789","type":"image","image":{"link":"https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"}}`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := BuildPayloadForMediaMessage(tt.args.options)
+
+			// print the json
+			t.Log(string(got))
+			if (err != nil) != tt.wantErr {
+				t.Errorf("BuildPayloadForMediaMessage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, []byte(tt.want)) {
+				t.Errorf("BuildPayloadForMediaMessage() = \n\n%v, want \n%v", got, []byte(tt.want))
+			}
+		})
+	}
 }
