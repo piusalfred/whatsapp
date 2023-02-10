8e1c8be31b4e7e85ef49d49b3a50c4357a7e81cd
Pius Alfred <me.pius1102@gmail.com>
2023-02-10 18:43:11 +0300

    setting up

--- examples/listener/listener.go
+++ examples/listener/listener.go
@@ -41,33 +41,33 @@ func (h *handler) HandleEvent(ctx context.Context, writer http.ResponseWriter, r
 //	curl -X POST --location "http://localhost:8080/webhooks" \
 //	   -H "Content-Type: application/json" \
 //	   -d "{
-//	         \"object\": \"whatsapp_business_account\",
-//	         \"entry\": [{
-//	           \"id\": \"WHATSAPP_BUSINESS_ACCOUNT_ID\",
-//	           \"changes\": [{
-//	             \"value\": {
-//	               \"messaging_product\": \"whatsapp\",
-//	               \"metadata\": {
-//	                 \"display_phone_number\": \"PHONE_NUMBER\",
-//	                 \"phone_number_id\": \"PHONE_NUMBER_ID\"
+//	         "object": "whatsapp_business_account",
+//	         "entry": [{
+//	           "id": "WHATSAPP_BUSINESS_ACCOUNT_ID",
+//	           "changes": [{
+//	             "value": {
+//	               "messaging_product": "whatsapp",
+//	               "metadata": {
+//	                 "display_phone_number": "PHONE_NUMBER",
+//	                 "phone_number_id": "PHONE_NUMBER_ID"
 //	               },
-//	               \"contacts\": [{
-//	                 \"profile\": {
-//	                   \"name\": \"NAME\"
+//	               "contacts": [{
+//	                 "profile": {
+//	                  	"name": "NAME"
 //	                 },
-//	                 \"wa_id\": \"PHONE_NUMBER_ID\"
+//	                 "wa_id": "PHONE_NUMBER_ID"
 //	               }],
-//	               \"messages\": [{
-//	                 \"from\": \"PHONE_NUMBER\",
-//	                 \"id\": \"wamid.ID\",
-//	                 \"timestamp\": \"TIMESTAMP\",
-//	                 \"text\": {
-//	                   \"body\": \"MESSAGE_BODY\"
+//	               "messages": [{
+//	                 "from": "PHONE_NUMBER",
+//	                 "id": "wamid.ID",
+//	                 "timestamp": "TIMESTAMP",
+//	                 "text": {
+//	                   "body": "MESSAGE_BODY"
 //	                 },
-//	                 \"type\": \"text\"
+//	                 "type": "text"
 //	               }]
 //	             },
-//	             \"field\": \"messages\"
+//	             "field": "messages"
 //	           }]
 //	         }]
 //	       }"
