ca9dd45530a99f35d6049b994d82d8747be30a9c
Pius Alfred <me.pius1102@gmail.com>
2023-02-15 15:47:20 +0300

    setting up qrcode

--- qrcode/qr.go
+++ qrcode/qr.go
@@ -1,128 +1,141 @@
-// Package qrcodes provides APi to manage QR codes.
-// It contains API oto create, update, get a list of, and delete QR Code Messages using
-// the WhatsApp Business Management API.
-//
-// Customers can scan a QR code from their phone to quickly begin a conversation with your
-// business. The WhatsApp Business Management API allows you to create and access these QR
-// codes and associated short links.
-//
-// If you can use the Business Manager to manage your QR codes instead of the API, see Manage
-// your WhatsApp QR Codes here https://web.facebook.com/business/help/890732351439459?_rdc=1&_rdr
-//
-// Before You Start
+/*
+Package qrcodes provides API implementations to manage QR codes for WhatsApp Business. It contains
+functions to create, retrieve, update and delete QR codes using the WhatsApp Business Management API.
 
-// You will need:
+Customers can scan a QR code from their phone to quickly begin a conversation with your
+business. The WhatsApp Business Management API allows you to create and access these QR
+codes and associated short links.
 
-// - The ID for the current phone number for your business (https://developers.facebook.com/docs/whatsapp/business-management-api/manage-phone-numbers#get-all-phone-numbers)
-// - A User access token requested by someone who is an admin for the Business Manager
-// - The whatsapp_business_messages permission
-//
-// Create a QR Code Message
+If you can use the Business Manager to manage your QR codes instead of the API, see Manage
+your WhatsApp QR Codes here https://web.facebook.com/business/help/890732351439459?_rdc=1&_rdr
 
-// To create a QR code for a business, send a POST request to the WhatsApp Business Phone Number >
-// Message Qrdls endpoint with the prefilled_message parameter set to your message text and
-// generate_qr_image parameter set to your preferred image format, either SVG or PNG.
+Before using this API in your application. You must have the following:
+  - The ID for the current phone number for your business (https://developers.facebook.com/docs/whatsapp/business-management-api/manage-phone-numbers#get-all-phone-numbers)
+  - A User access token requested by someone who is an admin for the Business Manager
+  - The whatsapp_business_messages permission
 
-// Example Request
+# Create a QR Code Message
 
-// Formatted for readability
-// curl -X POST "https://graph.facebook.com/v16.0/{phone-number-ID}/message_qrdls
-//   ?prefilled_message={message-text}
-//   &generate_qr_image={image-format}
-//   &access_token={user-access-token}"
-// On success, a JSON array is returned:
+Example:
 
-// {
-//     "code": "{qr-code-id}",
-//     "prefilled_message": "{business message text}",
-//     "deep_link_url": "{short-link-to-qr-code}",
-//     "qr_image_url": "{image-url}"
-// }
-// Retrieve a List of QR Code Messages
+	resp, err := qrcodes.Create(context.Background(), http.DefaultClient, "phoneNumberID","token", &qrcodes.CreateRequest{
+		PrefilledMessage: "Hello World",
+		ImageFormat:      qrcodes.ImageFormatPNG,
+	})
+	// handle error
+	......
+	// handle response
+	.......
 
-// To get a list of all the QR codes messages for a business, send a GET request to the WhatsApp Business Phone Number > Message Qrdls endpoint.
+This is equivalent to the following API call using curl which has been reformatted for readability:
 
-// Example Request
+	curl -X POST "https://graph.facebook.com/v16.0/{phone-number-ID}/message_qrdls
+	?prefilled_message="Hello World"
+	&generate_qr_image=PNG
+	&access_token={user-access-token}"
 
-// Formatted for readability
-// curl -X GET "https://graph.facebook.com/v16.0/{phone-number-ID}/message_qrdls
-//    &access_token={user-access-token}"
-// On success, a JSON array is returned:
+On success, a JSON array is returned:
 
-// {
-//     "data": [
-//         {
-//             "code": "ANOVZ6RINRD7G1",
-//             "prefilled_message": "I need help with my account.",
-//             "deep_link_url": "https://wa.me/message/ANOVZ6RINRD7G1"
-//         },
-//         {
-//             "code": "TNGSHG326AIHH1",
-//             "prefilled_message": "What are your store hours?",
-//             "deep_link_url": "https://wa.me/message/TNGSHG326AIHH1"
-//         },
-//         {
-//             "code": "R3LUI5KILJUYA1",
-//             "prefilled_message": "When is the next sale?",
-//             "deep_link_url": "https://wa.me/message/R3LUI5KILJUYA1"
-//         },
+	{
+		"code": "{qr-code-id}",
+		"prefilled_message": "{business message text}",
+		"deep_link_url": "{short-link-to-qr-code}",
+		"qr_image_url": "{image-url}"
+	}
 
-//     ]
-// }
-// Retrieve Single QR Code Message
+# Retrieve All QR Code Messages
 
-// To get information about a specific QR code message, send a GET request to the WhatsApp Business Phone Number > Message Qrdls endpoint and append the QR code ID as a path parameter.
+Example:
 
-// Example Request
+	resp, err := qrcodes.List(context.Background(), http.DefaultClient, "phoneNumberID","token")
+	// handle error
+	......
+	// handle response
+	......
 
-// Formatted for readability
-// curl -X GET "https://graph.facebook.com/v16.0/{phone-number-ID}/message_qrdls/{qr-code-id}
-//    &access_token={user-access-token}"
-// On success, a JSON array is returned:
+This is an equivalent to the following curl command:
 
-// {
-//     "data": [
-//         {
-//             "code": "ANOVZ6RINRD7G1",
-//             "prefilled_message": "I need help with my account.",
-//             "deep_link_url": "https://wa.me/message/ANOVZ6RINRD7G1"
-//         }
-//     ]
-// }
-// Update QR Code Message
+		curl -X GET "https://graph.facebook.com/v16.0/{phone-number-ID}/message_qrdls
+		    &access_token={user-access-token}"
+	 On success, a JSON array is returned:
 
-// To update a QR code for a business, send a POST request to the WhatsApp Business Phone Number > Message Qrdls endpoint and append the QR code ID as a path parameter.
+		{
+			"data": [
+				{
+					"code": "ANOVZ6RINRD7G1",
+					"prefilled_message": "I need help with my account.",
+					"deep_link_url": "https://wa.me/message/ANOVZ6RINRD7G1"
+				},
+				{
+					"code": "TNGSHG326AIHH1",
+	             "prefilled_message": "What are your store hours?",
+	             "deep_link_url": "https://wa.me/message/TNGSHG326AIHH1"
+	         },
+	         {
+	             "code": "R3LUI5KILJUYA1",
+	             "prefilled_message": "When is the next sale?",
+	             "deep_link_url": "https://wa.me/message/R3LUI5KILJUYA1"
+	         },
 
-// Example Request
+	     ]
+		}
 
-// Formatted for readability
-// curl -X POST "https://graph.facebook.com/v16.0/{phone-number-ID}/message_qrdls/{qr-code-id}
-//   ?prefilled_message={new-message-text}
-//   &access_token={user-access-token}"
-// On success, a JSON array is returned:
+# Retrive a single QR Code
 
-// {
-//     "code": "{qr-code-id}",
-//     "prefilled_message": "{business message text}",
-//     "deep_link_url": "{short-link-to-qr-code}"
-// }
-// Delete QR Code Message
+# Example Request
 
-// QR codes do not expire. You must delete a QR code in order to retire it.
+Formatted for readability
+curl -X GET "https://graph.facebook.com/v16.0/{phone-number-ID}/message_qrdls/{qr-code-id}
 
-// To delete a QR code, send a DELETE request to the WhatsApp Business Phone Number > Message Qrdls endpoint and append the ID of the QR code you wish to retire as a path parameter.
+	    &access_token={user-access-token}"
+	 On success, a JSON array is returned:
 
-// Example Request
+	 {
+	     "data": [
+	         {
+	             "code": "ANOVZ6RINRD7G1",
+	             "prefilled_message": "I need help with my account.",
+	             "deep_link_url": "https://wa.me/message/ANOVZ6RINRD7G1"
+	         }
+	     ]
+	}
 
-// Formatted for readability
-// curl -X DELETE "https://graph.facebook.com/v16.0/{phone-number-ID}/message_qrdls/{qr-code-id}
-//   &access_token={user-access-token}"
-// On success, a JSON array is returned:
+Example:
 
-//	{
-//	    "success": true
-//	}
-package qrcodes
+	info, err := qrcodes.Get(context.TODO(),http.DefaultClient, "baseURL", "phoneID", "accessToken", "qrCodeID")
+
+# Update Details of a QR Code
+
+# Example Request
+
+Formatted for readability
+
+	 curl -X POST "https://graph.facebook.com/v16.0/{phone-number-ID}/message_qrdls/{qr-code-id}
+	   ?prefilled_message={new-message-text}
+	   &access_token={user-access-token}"
+	 On success, a JSON array is returned:
+
+	 {
+	     "code": "{qr-code-id}",
+	     "prefilled_message": "{business message text}",
+	     "deep_link_url": "{short-link-to-qr-code}"
+	}
+
+Delete QR Code
+QR codes do not expire. You must delete a QR code in order to retire it.
+
+Formatted (for readability) curl command to delete a QR Code.
+curl -X DELETE "https://graph.facebook.com/v16.0/{phone-number-ID}/message_qrdls/{qr-code-id}
+
+	&access_token={user-access-token}"
+
+On success, a JSON array is returned:
+
+	{
+		"success": true
+	}
+*/
+package qrcodes // import "github.com/piusalfred/whatsapp/qrcodes"
 
 import (
 	"context"
