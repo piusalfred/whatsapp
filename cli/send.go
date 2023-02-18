18ddbef56d65161b15d76328a13c62883018f4a7
Pius Alfred <me.pius1102@gmail.com>
2023-02-18 15:15:07 +0300

    settings up

--- cli/send.go
+++ cli/send.go
@@ -12,6 +12,7 @@ type (
 	SendCommand struct {
 		SendTextCommand     SendTextCommand     `cmd:"" name:"text" help:"send a text message"`
 		SendLocationCommand SendLocationCommand `cmd:"" name:"location" help:"send a location message"`
+		SendTemplateCommand SendTemplateCommand `cmd:"" name:"template" help:"send a template message"`
 	}
 
 	SendTextCommand struct {
@@ -26,6 +27,13 @@ type (
 		Longitude float64 `name:"long" help:"the longitude of the location" type:"float64"`
 		Name      string  `name:"name" help:"the name of the location" type:"string"`
 		Address   string  `name:"address" help:"the address of the location" type:"string"`
+	}
+
+	SendTemplateCommand struct {
+		Recipient      string `name:"to" help:"the recipient of the message" type:"string"`
+		TemplateName   string `name:"template-name" help:"the name of the template" type:"string" default:"hello_world"`
+		LanguageCode   string `name:"language-code" help:"the language code of the template" type:"string" default:"en_US"`
+		LanguagePolicy string `name:"language-policy" help:"the language policy of the template" type:"string"`
 	}
 )
 
@@ -70,6 +78,27 @@ func (cmd *SendLocationCommand) Run(ctx *Context) error {
 	nctx, cancel := context.WithTimeout(ctx.ctx, time.Duration(ctx.Timeout)*time.Second)
 	defer cancel()
 	resp, err := whatsapp.SendLocation(nctx, ctx.http, req)
+	if err != nil {
+		return err
+	}
+	fmt.Println(resp)
+	return nil
+}
+
+func (cmd *SendTemplateCommand) Run(ctx *Context) error {
+	req := &whatsapp.SendTemplateRequest{
+		Recipient:              cmd.Recipient,
+		TemplateName:           cmd.TemplateName,
+		TemplateLanguageCode:   cmd.LanguageCode,
+		TemplateLanguagePolicy: cmd.LanguagePolicy,
+		ApiVersion:             ctx.ApiVersion,
+		BaseURL:                ctx.BaseURL,
+		PhoneNumberID:          ctx.PhoneID,
+		AccessToken:            ctx.AccessToken,
+	}
+	nctx, cancel := context.WithTimeout(ctx.ctx, time.Duration(ctx.Timeout)*time.Second)
+	defer cancel()
+	resp, err := whatsapp.SendTemplate(nctx, ctx.http, req)
 	if err != nil {
 		return err
 	}
