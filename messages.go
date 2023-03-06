da57e98e332f10cbc99b307fe5349235039900ec
Pius Alfred <me.pius1102@gmail.com>
2023-03-06 09:22:54 +0300

    go mod tidy

--- messages.go
+++ messages.go
@@ -47,45 +47,6 @@ type (
 	}
 )
 
-// MarkMessageRead sends a read receipt for a message.
-// When you receive an incoming message from Webhooks, you can use the /messages endpoint
-// to mark the message as read by changing its status to read. Messages marked as read
-// display two blue check marks alongside their timestamp:
-// We recommend marking incoming messages as read within 30 days of receipt. You cannot mark
-// outgoing messages you sent as read. Marking a message as read will also mark earlier
-// messages in the conversation as read.
-func MarkMessageRead(ctx context.Context, client *http.Client, url, token string) (*StatusResponse, error) {
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
-
-	q := req.URL.Query()
-	q.Add("access_token", token)
-	req.URL.RawQuery = q.Encode()
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("status code: %d", resp.StatusCode)
-	}
-
-	var result StatusResponse
-	err = json.NewDecoder(resp.Body).Decode(&result)
-	if err != nil {
-		return nil, err
-	}
-
-	return &result, nil
-}
-
 type SendTextRequest struct {
 	BaseURL       string
 	AccessToken   string
