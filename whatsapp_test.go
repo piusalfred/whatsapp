9f2a2e3bdeaf386975a8fdedbd32bbf94727a60c
Pius Alfred <me.pius1102@gmail.com>
2025-04-09 14:52:27 +0300

    add tests

--- whatsapp_test.go
+++ whatsapp_test.go
@@ -7,15 +7,15 @@ import (
 )
 
 func ExampleIsCorrectAPIVersion() {
-	fmt.Println(whatsapp.IsCorrectAPIVersion("v16.1"))    // true
+	fmt.Println(whatsapp.IsCorrectAPIVersion("v20.1"))    // true
 	fmt.Println(whatsapp.IsCorrectAPIVersion("v21.0"))    // true
 	fmt.Println(whatsapp.IsCorrectAPIVersion("v15.9"))    // false
 	fmt.Println(whatsapp.IsCorrectAPIVersion("v100.0"))   // true
 	fmt.Println(whatsapp.IsCorrectAPIVersion("v0.0"))     // false
 	fmt.Println(whatsapp.IsCorrectAPIVersion("v0.hello")) // false
 	fmt.Println(whatsapp.IsCorrectAPIVersion("vhi.1"))    // false
-	fmt.Println(whatsapp.IsCorrectAPIVersion("v16.0"))    // true
-	fmt.Println(whatsapp.IsCorrectAPIVersion("16.1"))     // false
+	fmt.Println(whatsapp.IsCorrectAPIVersion("v20.0"))    // true
+	fmt.Println(whatsapp.IsCorrectAPIVersion("20.1"))     // false
 
 	// Output:
 	// true
