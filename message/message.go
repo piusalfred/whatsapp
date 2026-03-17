2774cb1027bf108d82ec0d0574999ee750a08304
Sylvanus Kateile <kateile@outlook.com>
2026-03-17 20:07:09 +0300

    lower case field Status to status in message.go

--- message/message.go
+++ message/message.go
@@ -120,7 +120,7 @@ type (
 		Video           *Video           `json:"video,omitempty"`
 		Image           *Image           `json:"image,omitempty"`
 		Audio           *Audio           `json:"audio,omitempty"`
-		Status          *string          `json:"Status,omitempty"`     // used to update message Status
+		Status          *string          `json:"status,omitempty"`     // used to update message Status
 		MessageID       *string          `json:"message_id,omitempty"` // used to update message Status
 		Template        *Template        `json:"template,omitempty"`
 		TypingIndicator *TypingIndicator `json:"typing_indicator,omitempty"`
